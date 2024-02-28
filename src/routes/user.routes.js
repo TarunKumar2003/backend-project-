e0ef77752e38c7e050d86e2ece86e993eb108c55
Tarun Kumar <tarunnimbal123@gmail.com>
2024-02-28 12:07:41 +0530

    add user register and bug fixer

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,8 +2,22 @@
 
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import {upload} from "../middlewares/multer.middleware.js"
 const router = Router();
 
-router.route("/register").post(registerUser)
+router.route("/register").post(
+     // Multer is middleware it is injecting before the function that save the data into the database
+    upload.fields([
+        {
+            name:"avatar",
+            maxCount:1
+        },
+        {
+            name:coverImage,
+            maxCount:1
+        }
+    ]),
+    registerUser
+    )
 
 export default router;
